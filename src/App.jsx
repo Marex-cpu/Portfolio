9ced80c0e5074fc60343f628d1a6fdec68d573c0
marko-petrovic <markoo3871@gmail.com>
2022-12-13 12:27:57 +0100

    setup

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div className="container">
+        <p className="one">Hello</p>
+        <p className="two">Hi</p>
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/index.js
@@ -0,0 +1,21 @@
+import About from "./About";
+import Contact from "./Contact";
+import Experience from "./Experience";
+import Footer from "./Footer";
+import Header from "./Header";
+import Nav from "./Nav";
+import Portfolio from "./Portfolio";
+import Services from "./Services";
+import Testimonials from "./Testimonials";
+
+export {
+  About,
+  Contact,
+  Experience,
+  Footer,
+  Header,
+  Nav,
+  Portfolio,
+  Services,
+  Testimonials,
+};
